3ace1caeca008c78a9b9fe4b3c4f30124ad20f1d
Vitoria Natasha <98421932+vitori-a@users.noreply.github.com>
2022-06-09 18:06:28 -0300

    Add files via upload

---
+++ Chat/MensagemImagem.java
@@ -0,0 +1,11 @@
+package Chat;
+
+public class MensagemImagem extends Mensagem{
+    private String linkVideo;
+
+    public MensagemImagem() {
+        super(texto, reacao);
+        this.linkVideo = linkVideo;
+    }
+}
+}
---
+++ Chat/TesteMensagem.java
@@ -0,0 +1,9 @@
+package Chat;
+
+public class TesteMensagem {
+    public static void main(String[] args) {
+        Mensagem m1 = new Mensagem();
+
+        m1.reagir();
+    }
+}
